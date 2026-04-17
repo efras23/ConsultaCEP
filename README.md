f0a81723e274f12cdf93c1d5cb9a8ac83075fc0e
Efraim <efra10_efraim@hotmail.com>
2026-04-17 06:25:19 -0300

    feat: JavaScript, campos do formulário inicialmente ficam invisíveis

---
+++ README.md
@@ -0,0 +1,9 @@
+# Consulta de Endereço (por CEP)
+
+Consulta de endereço no Brasil com base no CEP digitado!
+
+---
+
+## Características
+
+- Implementação de "máscara de input" no CEP, garantindo sua correta formatação (XX.XXX-XXX);
---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const queryBtn = document.getElementById("consultar");
+    
+    // Deixar campos VISÍVEIS
+    queryBtn.addEventListener("click", function(event){
+        event.preventDefault(); // Impedir página de recarregar
+
+        const formDivs =  document.querySelectorAll("form fieldset div:not(:first-of-type)");
+        formDivs.forEach(div => {
+            div.classList.add("visible");
+        })
+    })
+
+    const inputCEP = document.getElementById("cep");
+
+    // Máscara de CEP (XX.XXX-XXX)
+    inputCEP.addEventListener("input", function(){
+        let cepAtual = inputCEP.value;
+
+        // REGEX 
+        cepAtual = cepAtual.replace(/\D/g, ""); // Remove caracteres NÃO numéricos
+
+        cepAtual = cepAtual.replace(/^(\d{2})(\d)/, "$1.$2"); // Adiciona ponto após 2 dígitos
+
+        cepAtual = cepAtual.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2-$3"); // Adiciona hífen após 5 dígitos
+
+        inputCEP.value = cepAtual;
+    })
+})
