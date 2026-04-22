ec4022ccdcb4283484ddfe1807838ef23d0d02d9
Efraim <efra10_efraim@hotmail.com>
2026-04-22 08:25:55 -0300

    feat: função de validação de CEP (JavaScript)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
+function isValidCep(inputCEP){
+    return /^(\d{2})\.(\d{3})-(\d{3})$/.test(inputCEP.value);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
+
     const queryBtn = document.getElementById("consultar");
     
-    // Deixar campos VISÍVEIS
+    // Tornar campos VISÍVEIS
     queryBtn.addEventListener("click", function(event){
         event.preventDefault(); // Impedir página de recarregar
 
@@ -17,13 +22,20 @@ document.addEventListener("DOMContentLoaded", function(){
     inputCEP.addEventListener("input", function(){
         let cepAtual = inputCEP.value;
 
-        // REGEX 
-        cepAtual = cepAtual.replace(/\D/g, ""); // Remove caracteres NÃO numéricos
-
-        cepAtual = cepAtual.replace(/^(\d{2})(\d)/, "$1.$2"); // Adiciona ponto após 2 dígitos
-
+        // REGEX
+        cepAtual = cepAtual.replace(/\D/g, "");                           // Remove caracteres NÃO numéricos
+        cepAtual = cepAtual.replace(/^(\d{2})(\d)/, "$1.$2");             // Adiciona ponto após 2 dígitos
         cepAtual = cepAtual.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2-$3"); // Adiciona hífen após 5 dígitos
 
         inputCEP.value = cepAtual;
+    })
+    
+    // Consultar CEP
+    inputCEP.addEventListener("blur", function(){
+        if (isValidCep(inputCEP)){
+            const cep = inputCEP.value.replace(/\D/g, ""); // Apenas dígitos
+            console.log(inputCEP.value + " " + inputCEP.value.length + " chars.");
+            console.log(cep + " " + cep.length + " chars");
+        }
     })
 })
